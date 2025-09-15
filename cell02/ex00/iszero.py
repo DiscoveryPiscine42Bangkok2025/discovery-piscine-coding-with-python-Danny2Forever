c4f4473b4d891a8ef0f5dd376bd68b281a46d502
Lattae <105736002+Danny2Forever@users.noreply.github.com>
2025-09-16 00:41:29 +0700

    add iszero

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,11 @@
+'''docstring'''
+def iszero(num):
+    '''docstring'''
+    print("This number is different from zero.") if num else print("This number is equal to zero.")
+
+def main():
+    '''docstring'''
+    num = input()
+    iszero(int(num))
+
+main()
